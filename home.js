d1a914a4a2adbf7d7f3ef346bb28c020d515b958
NithyaKanamarlapudi <76540341+NithyaKanamarlapudi@users.noreply.github.com>
2022-08-15 20:57:41 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+function bed() {
+    window.location = "bed.html";
+  }
+function counter() {
+    window.location = "counter.html";
+  }
+function desk() {
+    window.location = "desk.html";
+  }
+function piano() {
+    window.location = "piano.html";
+  }
+function tv() {
+    window.location = "tv.html";
+  }
